2f2b17b542fffafbe638e02eb9c65a5a19d0ea58
guicardoso0404 <guilhermecardoso0404@gmail.com>
2025-12-25 18:46:37 -0300

    fix: add headers to disable caching for post feed response

--- controllers/postController.js
+++ controllers/postController.js
@@ -110,6 +110,12 @@ class PostController {
                 post.comentarios_lista = comments;
             }
             
+            // Adicionar headers para desabilitar o cache
+            res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+            res.setHeader('Pragma', 'no-cache');
+            res.setHeader('Expires', '0');
+            res.setHeader('Surrogate-Control', 'no-store');
+
             res.json({ success: true, data: posts });
             
         } catch (error) {
